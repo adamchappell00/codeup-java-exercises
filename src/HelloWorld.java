ef37cc3ef64270988ee09350e0bfa3617e4f02a9
adamchappell00 <adam.chappell00@gmail.com>
2021-12-14 15:11:26 -0600

    first exerise complete

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,45 @@ public class HelloWorld {
     public static void main(String[] args){
         System.out.print("Hello ");
         System.out.print("World!");
+
+        int myFavoriteNumber = 13;
+        System.out.println(myFavoriteNumber);
+        String myString = "Woohoo, it's my string!";
+        // String myString = 'w';
+        // String myString = 3.14159;
+        System.out.println(myString);
+
+        //long myNumber;
+        //long myNumber = 123L;
+        //long myNumber = 123;
+        float myNumber = (float) 3.14;
+        System.out.println(myNumber);
+
+        int x = 5;
+        System.out.println(x++);
+        System.out.println(x);
+
+        int y = 5;
+        System.out.println(++y);
+        System.out.println(y);
+
+        //String class = "Yup.";
+
+        //String theNumberThree = "three";
+        //Object o = theNumberThree;
+        //int three = (int) o;
+        //int three = (int) "three";
+        //System.out.println(three);
+
+        int z = 4;
+        z += 5;
+        System.out.println(z);
+
+        int a = 3;
+        int b = 4;
+        b *= a;
+        System.out.println(b);
+
+
     }
 }
