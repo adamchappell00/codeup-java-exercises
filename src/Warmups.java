7e93f1e95a67c4daca4a5c88544f4cf88cc97021
adamchappell00 <adam.chappell00@gmail.com>
2021-12-15 09:04:30 -0600

    first java warmup complete

---
+++ src/Warmups.java
@@ -0,0 +1,7 @@
+public class Warmups {
+    public static void main(String[] args){
+
+        // First Warmup: 15 December
+        System.out.println("Java is statically typed, while Javascript is dynamically typed.");
+    }
+}
