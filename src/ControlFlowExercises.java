5710f7367cf181a6db061b558ecaadebfcd21a7e
adamchappell00 <adam.chappell00@gmail.com>
2021-12-15 11:00:05 -0600

    Control Statements and Loops Started

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,14 @@
+public class ControlFlowExercises {
+    public static void main(String[] args){
+
+        // Exercise Part 1
+        /*
+        int i = 5;
+        while(i <= 15){
+            System.out.println(i++);
+        }
+         */
+        // Exercise Part 2
+
+    }
+}
