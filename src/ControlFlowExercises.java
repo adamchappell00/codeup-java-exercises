9f132f676a000913af12ec1bc31308b7c0373c8c
adamchappell00 <adam.chappell00@gmail.com>
2021-12-16 09:04:41 -0600

    16 December Warmup complete

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,14 +1,14 @@
 public class ControlFlowExercises {
     public static void main(String[] args){
 
-        // Exercise Part 1
+        // Exercise 1.a
         /*
         int i = 5;
         while(i <= 15){
             System.out.println(i++);
         }
          */
-        // Exercise Part 2
+        // Exercise 1.b
 
     }
 }
--- src/Warmups.java
+++ src/Warmups.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
+
 public class Warmups {
     public static void main(String[] args){
 
+
+        Scanner scanner = new Scanner(System.in);
+
+        // Warmup 16 December
+        System.out.println("What did you eat for breakfast today?");
+        String userBreakfast = scanner.nextLine();
+        System.out.printf("%s sounds great!", userBreakfast);
+
         // First Warmup: 15 December
-        System.out.println("Java is statically typed, while Javascript is dynamically typed.");
+        //System.out.println("Java is statically typed, while Javascript is dynamically typed.");
+
+
     }
 }
