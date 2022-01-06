6975e9a80141bd38bbdec61e6dd7440cc23792bb
adamchappell00 <adam.chappell00@gmail.com>
2022-01-06 15:33:13 -0600

    Arrays Exercises Startup

---
+++ src/ArraysExercises.java
@@ -0,0 +1,7 @@
+public class ArraysExercises {
+    public static void main(String[] args) {
+        Person[] people = {new Person("Bob"), new Person("Steve"), new Person("John"), new Person("James")};
+
+
+    }
+}
---
+++ src/ServerNameGenerator.java
@@ -0,0 +1,9 @@
+public class ServerNameGenerator {
+    public static void main(String[] args) {
+        String[] adjectives = {"Big","Small","Dark","Awesome","Wonderful","Colorful","Good","Square","Round","Bright"};
+        String[] nouns = {"Taco","Table","Chair","Shirt","Book","Bottle","Car","Building","Bus","Bicycle"};
+    }
+    public String getRandom(){
+
+    }
+}
